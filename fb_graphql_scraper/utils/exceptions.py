4610c9ecadf04b88be1241f2fcdab89147674b1c
TheFreeMan360 <54360yes@gmail.com>
2024-04-07 21:59:07 +0800

    Update driver not support excpetons

--- fb_graphql_scraper/utils/exceptions.py
+++ fb_graphql_scraper/utils/exceptions.py
@@ -1,2 +1,12 @@
+# -*- coding: utf-8 -*-
+class DriverTypeNotSupportException(Exception):
+    def __init__(self, driver_type: str) -> None:
+        self.__driver_type = driver_type
+
+    def __str__(self) -> str:
+        # return f"Browser name : {self.__browser} are not Support, please choose chrome, firefox, or edge."
+        return f"Driver type : {self.__driver_type} are not Support, please choose chrome."
+
+
 class NoIDException():
     pass
